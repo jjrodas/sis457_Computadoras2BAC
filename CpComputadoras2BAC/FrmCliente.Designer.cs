d07728cb931ded5cb73ece945199e4e6a4308b1f
Juan Jose Rodas Paco <geometrycalero030400@gmail.com>
2023-11-16 05:29:13 -0400

    Modificaciones FrmCliente.

---
+++ CpComputadoras2BAC/FrmCliente.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace CpComputadoras2BAC
+{
+    partial class FrmCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxDatos = new System.Windows.Forms.GroupBox();
+            this.txtprimerApellido = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.pnlAcciones = new System.Windows.Forms.Panel();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.gbxLista = new System.Windows.Forms.GroupBox();
+            this.dgvListaClientes = new System.Windows.Forms.DataGridView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lblBusqueda = new System.Windows.Forms.Label();
+            this.txtParametro = new System.Windows.Forms.TextBox();
+            this.lblPrincipal = new System.Windows.Forms.Label();
+            this.txtcedulaIdentidad = new System.Windows.Forms.TextBox();
+            this.lblCedulaIdentidad = new System.Windows.Forms.Label();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.lblCelular = new System.Windows.Forms.Label();
+            this.txtsegundoApellido = new System.Windows.Forms.TextBox();
+            this.gbxDatos.SuspendLayout();
+            this.pnlAcciones.SuspendLayout();
+            this.gbxLista.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxDatos
+            // 
+            this.gbxDatos.Controls.Add(this.txtsegundoApellido);
+            this.gbxDatos.Controls.Add(this.txtCelular);
+            this.gbxDatos.Controls.Add(this.lblCelular);
+            this.gbxDatos.Controls.Add(this.txtcedulaIdentidad);
+            this.gbxDatos.Controls.Add(this.lblCedulaIdentidad);
+            this.gbxDatos.Controls.Add(this.txtprimerApellido);
+            this.gbxDatos.Controls.Add(this.txtNombres);
+            this.gbxDatos.Controls.Add(this.lblDescripcion);
+            this.gbxDatos.Controls.Add(this.lblNombre);
+            this.gbxDatos.Controls.Add(this.btnCancelar);
+            this.gbxDatos.Controls.Add(this.btnGuardar);
+            this.gbxDatos.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxDatos.Location = new System.Drawing.Point(75, 402);
+            this.gbxDatos.Name = "gbxDatos";
+            this.gbxDatos.Size = new System.Drawing.Size(790, 141);
+            this.gbxDatos.TabIndex = 34;
+            this.gbxDatos.TabStop = false;
+            this.gbxDatos.Text = "Detalles del cliente";
+            // 
+            // txtprimerApellido
+            // 
+            this.txtprimerApellido.Location = new System.Drawing.Point(152, 98);
+            this.txtprimerApellido.MaxLength = 250;
+            this.txtprimerApellido.Name = "txtprimerApellido";
+            this.txtprimerApellido.Size = new System.Drawing.Size(260, 26);
+            this.txtprimerApellido.TabIndex = 25;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(152, 60);
+            this.txtNombres.MaxLength = 250;
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(260, 26);
+            this.txtNombres.TabIndex = 24;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(8, 101);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(69, 19);
+            this.lblDescripcion.TabIndex = 14;
+            this.lblDescripcion.Text = "Apellidos:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(8, 67);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(69, 19);
+            this.lblNombre.TabIndex = 8;
+            this.lblNombre.Text = "Nombres:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(682, 89);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(102, 42);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGuardar.Location = new System.Drawing.Point(581, 89);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(95, 42);
+            this.btnGuardar.TabIndex = 11;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // pnlAcciones
+            // 
+            this.pnlAcciones.Controls.Add(this.btnCerrar);
+            this.pnlAcciones.Controls.Add(this.btnEliminar);
+            this.pnlAcciones.Controls.Add(this.btnEditar);
+            this.pnlAcciones.Controls.Add(this.btnNuevo);
+            this.pnlAcciones.Location = new System.Drawing.Point(76, 327);
+            this.pnlAcciones.Name = "pnlAcciones";
+            this.pnlAcciones.Size = new System.Drawing.Size(789, 48);
+            this.pnlAcciones.TabIndex = 33;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCerrar.Location = new System.Drawing.Point(501, 3);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(87, 42);
+            this.btnCerrar.TabIndex = 10;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(402, 3);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(93, 42);
+            this.btnEliminar.TabIndex = 9;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Enabled = false;
+            this.btnEditar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEditar.Location = new System.Drawing.Point(309, 3);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(87, 42);
+            this.btnEditar.TabIndex = 8;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNuevo.Location = new System.Drawing.Point(212, 3);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(91, 42);
+            this.btnNuevo.TabIndex = 7;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // gbxLista
+            // 
+            this.gbxLista.Controls.Add(this.dgvListaClientes);
+            this.gbxLista.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxLista.Location = new System.Drawing.Point(77, 92);
+            this.gbxLista.Name = "gbxLista";
+            this.gbxLista.Size = new System.Drawing.Size(790, 224);
+            this.gbxLista.TabIndex = 32;
+            this.gbxLista.TabStop = false;
+            this.gbxLista.Text = "Lista de clientes";
+            // 
+            // dgvListaClientes
+            // 
+            this.dgvListaClientes.AllowUserToAddRows = false;
+            this.dgvListaClientes.AllowUserToDeleteRows = false;
+            this.dgvListaClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvListaClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListaClientes.Location = new System.Drawing.Point(7, 22);
+            this.dgvListaClientes.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvListaClientes.MultiSelect = false;
+            this.dgvListaClientes.Name = "dgvListaClientes";
+            this.dgvListaClientes.ReadOnly = true;
+            this.dgvListaClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvListaClientes.Size = new System.Drawing.Size(776, 195);
+            this.dgvListaClientes.TabIndex = 0;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBuscar.Location = new System.Drawing.Point(776, 50);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(91, 42);
+            this.btnBuscar.TabIndex = 31;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // lblBusqueda
+            // 
+            this.lblBusqueda.AutoSize = true;
+            this.lblBusqueda.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBusqueda.Location = new System.Drawing.Point(77, 45);
+            this.lblBusqueda.Name = "lblBusqueda";
+            this.lblBusqueda.Size = new System.Drawing.Size(193, 19);
+            this.lblBusqueda.TabIndex = 30;
+            this.lblBusqueda.Text = "Buscar por nombre del cliente:";
+            // 
+            // txtParametro
+            // 
+            this.txtParametro.Location = new System.Drawing.Point(77, 64);
+            this.txtParametro.Name = "txtParametro";
+            this.txtParametro.Size = new System.Drawing.Size(688, 20);
+            this.txtParametro.TabIndex = 29;
+            this.txtParametro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtParametro_KeyPress);
+            // 
+            // lblPrincipal
+            // 
+            this.lblPrincipal.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrincipal.Location = new System.Drawing.Point(18, 12);
+            this.lblPrincipal.Name = "lblPrincipal";
+            this.lblPrincipal.Size = new System.Drawing.Size(909, 34);
+            this.lblPrincipal.TabIndex = 28;
+            this.lblPrincipal.Text = "Clientes";
+            this.lblPrincipal.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtcedulaIdentidad
+            // 
+            this.txtcedulaIdentidad.Location = new System.Drawing.Point(152, 25);
+            this.txtcedulaIdentidad.MaxLength = 250;
+            this.txtcedulaIdentidad.Name = "txtcedulaIdentidad";
+            this.txtcedulaIdentidad.Size = new System.Drawing.Size(260, 26);
+            this.txtcedulaIdentidad.TabIndex = 36;
+            // 
+            // lblCedulaIdentidad
+            // 
+            this.lblCedulaIdentidad.AutoSize = true;
+            this.lblCedulaIdentidad.Location = new System.Drawing.Point(8, 32);
+            this.lblCedulaIdentidad.Name = "lblCedulaIdentidad";
+            this.lblCedulaIdentidad.Size = new System.Drawing.Size(135, 19);
+            this.lblCedulaIdentidad.TabIndex = 35;
+            this.lblCedulaIdentidad.Text = "Cédula de Identidad:";
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(524, 29);
+            this.txtCelular.MaxLength = 250;
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(260, 26);
+            this.txtCelular.TabIndex = 38;
+            // 
+            // lblCelular
+            // 
+            this.lblCelular.AutoSize = true;
+            this.lblCelular.Location = new System.Drawing.Point(452, 32);
+            this.lblCelular.Name = "lblCelular";
+            this.lblCelular.Size = new System.Drawing.Size(55, 19);
+            this.lblCelular.TabIndex = 37;
+            this.lblCelular.Text = "Celular:";
+            // 
+            // txtsegundoApellido
+            // 
+            this.txtsegundoApellido.Location = new System.Drawing.Point(524, 64);
+            this.txtsegundoApellido.MaxLength = 250;
+            this.txtsegundoApellido.Name = "txtsegundoApellido";
+            this.txtsegundoApellido.Size = new System.Drawing.Size(260, 26);
+            this.txtsegundoApellido.TabIndex = 39;
+            // 
+            // FrmCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(945, 554);
+            this.Controls.Add(this.gbxDatos);
+            this.Controls.Add(this.pnlAcciones);
+            this.Controls.Add(this.gbxLista);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.lblBusqueda);
+            this.Controls.Add(this.txtParametro);
+            this.Controls.Add(this.lblPrincipal);
+            this.Name = "FrmCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "::: Clientes - CompumundoBAC :::";
+            this.Load += new System.EventHandler(this.FrmCliente_Load);
+            this.gbxDatos.ResumeLayout(false);
+            this.gbxDatos.PerformLayout();
+            this.pnlAcciones.ResumeLayout(false);
+            this.gbxLista.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxDatos;
+        private System.Windows.Forms.TextBox txtcedulaIdentidad;
+        private System.Windows.Forms.Label lblCedulaIdentidad;
+        private System.Windows.Forms.TextBox txtprimerApellido;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Panel pnlAcciones;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.GroupBox gbxLista;
+        private System.Windows.Forms.DataGridView dgvListaClientes;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblBusqueda;
+        private System.Windows.Forms.TextBox txtParametro;
+        private System.Windows.Forms.Label lblPrincipal;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.Label lblCelular;
+        private System.Windows.Forms.TextBox txtsegundoApellido;
+    }
+}
---
+++ CpComputadoras2BAC/FrmCliente.cs
@@ -0,0 +1,73 @@
+﻿using ClnComputadoras2BAC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpComputadoras2BAC
+{
+    public partial class FrmCliente : Form
+    {
+        bool esNuevo = false;
+        public FrmCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void listar()
+        {
+            var clientes = ClienteCln.listarPa(txtParametro.Text.Trim());
+            dgvListaClientes.DataSource = clientes;
+            dgvListaClientes.Columns["id"].Visible = false;
+            dgvListaClientes.Columns["estado"].Visible = false;
+            dgvListaClientes.Columns["ci"].HeaderText = "Cédula de Identidad";
+            dgvListaClientes.Columns["nombres"].HeaderText = "Nombres";
+            dgvListaClientes.Columns["primerApellido"].HeaderText = "Primer Apellido";
+            dgvListaClientes.Columns["segundoApellido"].HeaderText = "Segundo Apellido";
+            dgvListaClientes.Columns["celular"].HeaderText = "Celular";
+            dgvListaClientes.Columns["usuarioRegistro"].HeaderText = "Usuario";
+            dgvListaClientes.Columns["fechaRegistro"].HeaderText = "Fecha de Registro";
+            btnEditar.Enabled = clientes.Count > 0;
+            btnEliminar.Enabled = clientes.Count > 0;
+            if (clientes.Count > 0) dgvListaClientes.Rows[0].Cells["nombre"].Selected = true;
+        }
+
+        private void txtParametro_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) listar();
+        }
+
+        private void FrmCliente_Load(object sender, EventArgs e)
+        {
+            Size = new Size(961, 431);
+            listar();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            esNuevo = true;
+            Size = new Size(961, 593);
+            txtcedulaIdentidad.Focus();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            esNuevo = false;
+            Size = new Size(961, 593);
+
+            int index = dgvListaClientes.CurrentCell.RowIndex;
+            int id = Convert.ToInt32(dgvListaClientes.Rows[index].Cells["id"].Value);
+            var cliente = ClienteCln.get(id);
+            txtcedulaIdentidad.Text = cliente.cedulaIdentidad;
+            txtNombres.Text = cliente.nombres;
+            txtprimerApellido.Text = cliente.primerApellido;
+            txtsegundoApellido.Text = cliente.segundoApellido;
+            txtCelular.Text = cliente.celular;
+        }
+    }
+}
